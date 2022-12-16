160e2d538d66e2d979619d6c32a6efbbf14e45fe
karla8marta <95685389+karla8marta@users.noreply.github.com>
2022-12-16 12:35:23 +0000

    Update worker.py

--- worker.py
+++ worker.py
@@ -20,7 +20,7 @@ def hello():
 
 @app.route("/test")
 def test():
-    #return "Test" # testing again
+    #return "Test" # testing again2
     return(get_api_key())
 
 @app.route("/add",methods=['GET','POST'])
